b9c2087d8cc9f2f50cd8dfc7544b2c87119afd76
Ryan Fallon <10779802+ryano0oceros@users.noreply.github.com>
2023-04-06 18:42:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,75 @@
-///Wrong implementation
+const express = require('express');
+const config = require('../config')
+const router = express.Router()
 
-//unvalidated "clientName" parameter appended to the query allows an attacker to inject any SQL code
+const MongoClient = require('mongodb').MongoClient;
+const url = config.MONGODB_URI;
 
-String query = "SELECT account_balance FROM user_data WHERE user_name = "
-             + request.getParameter("clientName");
-try {
-    Statement statement = connection.createStatement( ... );
-    ResultSet results = statement.executeQuery( query );
-}
+router.post('/customers/register', async (req, res) => {
+
+    const client = await MongoClient.connect(url, { useNewUrlParser: true })
+        .catch(err => { console.log(err); });
+    if (!client) {
+        return res.json({ status: "Error" });
+    }
+    const db = client.db(config.MONGODB_DB_NAME);
+    const customers = db.collection("customers")
+    
+    let myobj = { name: req.body.name, address: req.body.address };
+    customers.insertOne(myobj, function (err) {
+        if (err) throw err;
+        console.log("user registered");
+        res.json({ status:"success", "message": "user inserted" })
+        db.close();
+    });
+    
+})
+
+
+// Vulnerable search function
+router.post('/customers/find', async (req, res) => {
+
+    const client = await MongoClient.connect(url, { useNewUrlParser: true })
+        .catch(err => { console.log(err); });
+    if (!client) {
+        return res.json({ status: "Error" });
+    }
+    const db = client.db(config.MONGODB_DB_NAME);
+    const customers = db.collection("customers")
+
+    let name = req.body.name
+    let myobj = { name: name };
+    customers.findOne(myobj, function (err, result) {
+        if (err) throw err;
+        db.close();
+        res.json(result)
+    });
+
+  
+})
+
+// Vulnerable Authentication
+// Authentication Bypass Example
+// curl -X POST http://localhost:3000/customers/login/ --data "{\"email\": {\"\$gt\":\"\"} , \"password\": {\"\$gt\":\"\"}}" -H "Content-Type: application/json"
+
+router.post('/customers/login', async (req, res) => {
+
+    const client = await MongoClient.connect(url, { useNewUrlParser: true })
+        .catch(err => { console.log(err); });
+    if (!client) {
+        return res.json({ status: "Error" });
+    }
+    const db = client.db(config.MONGODB_DB_NAME);
+    const customers = db.collection("customers")
+
+    let myobj = { email: req.body.email, password: req.body.password };
+    customers.findOne(myobj, function (err, result) {
+        if (err) throw err;
+        db.close();
+        res.json(result)
+    });
+
+ 
+})
+
+module.exports = router
